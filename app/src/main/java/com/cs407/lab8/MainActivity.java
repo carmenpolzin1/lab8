d298141077fc52af05a68ea6f231ba911e80e16e
carmenpolzin1 <cpolzin@wisc.edu>
2023-10-31 14:44:46 -0500

    milestone 1

--- app/src/main/java/com/cs407/lab8/MainActivity.java
+++ app/src/main/java/com/cs407/lab8/MainActivity.java
@@ -9,6 +9,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+..test
 
 public class MainActivity extends AppCompatActivity {
 
